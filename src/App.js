01c76ab0f63720c50b7cb0f544e8bcfe6bf5f9f7
Nikola <nikola.dj.87@gmail.com>
2022-07-01 13:27:45 +0200

    new video and no menu

--- src/App.js
+++ src/App.js
@@ -1,37 +1,31 @@
 import "./App.scss";
-import { Routes, Route } from "react-router-dom";
 import About from "./pages/about/About";
 import Home from "./pages/home/Home";
 import Projects from "./pages/projects/Projects";
-import SideMenu from "./components/sidebarMenu/SidebarMenu";
-import video from "./components/video/2.mp4";
+import video from "./components/video/3.mp4";
 
 function App() {
   return (
     <div className="App">
+      <video id="bgVid" autoPlay loop muted>
+        <source src={video} type="video/mp4" />
+      </video>
       <div className="topEdge">
         <img
           src="https://raw.githubusercontent.com/DjordjevicN/imagesRep/master/Vector.png"
           alt=""
         />
       </div>
-      <video id="bgVid" autoPlay loop muted>
-        <source src={video} type="video/mp4" />
-      </video>
+      <div className="content">
+        <Home />
+        <About />
+        <Projects />
+      </div>
       <div className="bottomEdge">
         <img
           src="https://raw.githubusercontent.com/DjordjevicN/imagesRep/master/Vector.png"
           alt=""
         />
-      </div>
-
-      <SideMenu />
-      <div className="pages">
-        <Routes>
-          <Route index path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="projects" element={<Projects />} />
-        </Routes>
       </div>
     </div>
   );

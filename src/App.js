874bd2b42db64426dd2f031b933cfe2ce242cff3
Nikola Djordjevic <nikoladjordjevic@Nikolas-MacBook-Pro.local>
2023-02-19 23:08:17 +0100

    unused vars

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import About from "./pages/about/About"
 import Home from "./pages/home/Home"
 import Projects from "./pages/projects/Projects"
 import video from "./components/video/3.mp4"
-import CyberBtn from "./components/button/CyberBtn"
-import { motion } from "framer-motion"
+// import CyberBtn from "./components/button/CyberBtn"
+// import { motion } from "framer-motion"
 import Intro from "./pages/IntroStory/Intro"
 
 function App() {
-  const cvBtn = {
-    open: {
-      opacity: 1,
-      right: 30,
-    },
-  }
+  // const cvBtn = {
+  //   open: {
+  //     opacity: 1,
+  //     right: 30,
+  //   },
+  // }
 
   return (
     <div className="App">

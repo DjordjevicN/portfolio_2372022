4f34868aef49960bcefc5c55adc9742f341d1ef8
Nikola Djordjevic <nikoladjordjevic@Nikolas-MacBook-Pro.local>
2023-02-20 00:47:45 +0100

    responsive for story

--- src/pages/IntroStory/Intro.js
+++ src/pages/IntroStory/Intro.js
@@ -2,25 +2,20 @@ import React, { useEffect, useState } from "react"
 import "./Intro.scss"
 
 const introText = [
-  { id: 0, text: "HI!", glitch: false, duration: 3000 },
-  { id: 1, text: "", glitch: false, duration: 1000 },
-  { id: 2, text: "I'm Nikola Đorđević", glitch: true, duration: 2000 },
-  { id: 3, text: "", glitch: false, duration: 1000 },
-  { id: 4, text: "I'm a Frontend Developer", glitch: false, duration: 2000 },
-  { id: 5, text: "", glitch: false, duration: 1000 },
-  { id: 6, text: "Welcome to my portfolio!", glitch: true, duration: 2000 },
-  { id: 7, text: "", glitch: false, duration: 1000 },
-  {
-    id: 8,
-    text: "Let me guide you through some of my work",
-    glitch: false,
-    duration: 2000,
-  },
-  { id: 5, text: "", glitch: false, duration: 3000 },
+  { id: 0, text: "", glitch: false, duration: 100 },
+  { id: 1, text: "HI!", glitch: false, duration: 3000 },
+  { id: 2, text: "", glitch: false, duration: 1000 },
+  { id: 3, text: "I'm Nikola Đorđević", glitch: true, duration: 2000 },
+  { id: 4, text: "", glitch: false, duration: 1000 },
+  { id: 5, text: "I'm a Frontend Developer", glitch: false, duration: 2000 },
+  { id: 6, text: "", glitch: false, duration: 1000 },
+  { id: 7, text: "Welcome to my portfolio!", glitch: true, duration: 2000 },
+  { id: 8, text: "", glitch: false, duration: 1000 },
 ]
 
 const Intro = () => {
   const [index, setIndex] = useState(0)
+  const [showIntro, setShowIntro] = useState(true)
 
   const disableScroll = () => {
     document.body.style.overflow = "hidden"
@@ -30,13 +25,6 @@ const Intro = () => {
     document.body.style.overflow = "auto"
   }
 
-  const hideIntro = () => {
-    setTimeout(() => {
-      const introWrapper = document.querySelector(".introWrapper")
-      introWrapper.style.display = "none"
-    }, 1000)
-  }
-
   const scrollDown = () => {
     window.scroll({
       top: window.innerHeight,
@@ -44,7 +32,15 @@ const Intro = () => {
     })
   }
 
+  const scrollUp = () => {
+    window.scroll({
+      top: 0,
+      behavior: "smooth",
+    })
+  }
+
   useEffect(() => {
+    scrollUp()
     let timerId
     if (index < introText.length - 1) {
       disableScroll()
@@ -52,9 +48,12 @@ const Intro = () => {
         setIndex(index + 1)
       }, introText[index].duration || 2000)
     } else {
+      console.warn("end of intro")
       enableScroll()
       scrollDown()
-      hideIntro()
+      setTimeout(() => {
+        setShowIntro(false)
+      }, 1000)
     }
     return () => {
       clearTimeout(timerId)
@@ -62,17 +61,21 @@ const Intro = () => {
   }, [index])
 
   return (
-    <div className="introWrapper">
-      <div className="introContent">
-        <div className="introText">
-          <div className="introTextItem">
-            <h1 className={introText[index].glitch ? "glitch" : ""}>
-              {introText[index].text}
-            </h1>
+    <>
+      {showIntro && (
+        <div className="introWrapper">
+          <div className="introContent">
+            <div className="introText">
+              <div className="introTextItem">
+                <h1 className={introText[index].glitch ? "glitch" : ""}>
+                  {introText[index].text}
+                </h1>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   )
 }
 
